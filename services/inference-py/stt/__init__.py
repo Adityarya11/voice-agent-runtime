63b5e301893d43448ff7f13e3a8c3a26bb27ad1a
Adityarya11 <arya050411@gmail.com>
2026-07-05 11:37:25 +0530

    added file as a package for all modules

---
+++ services/inference-py/stt/__init__.py
@@ -0,0 +1,5 @@
+from .transcriber import Transcriber
+
+__all__ = [
+    "Transcriber"
+]
--- services/inference-py/vad/__init__.py
+++ services/inference-py/vad/__init__.py
@@ -1,7 +1,11 @@
 from .detector import VADCommand, VADState, VADDetector
+from .audio import frames_to_wav
+from .preprocessor import AudioPreprocessor
 
 __all__ = [
     "VADCommand",
     "VADState",
     "VADDetector",
+    "AudioPreprocessor", 
+    "frames_to_wav"
 ]
